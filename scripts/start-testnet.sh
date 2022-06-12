242965e8aa416dc297394e3f79ddbe7bb079e985
zongxiong.chen <czxczf@gmail.com>
2022-06-12 21:15:28 +0200

    fix typo

--- scripts/start-testnet.sh
+++ scripts/start-testnet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 $HOME/.cargo/bin/cargo build --release
-ln -sf $HOME/actions-runner/_work/governance2-testnet/governance2-testnet/release/governance2 $HOME/governance2
+ln -sf $HOME/actions-runner/_work/governance2-testnet/governance2-testnet/target/release/governance2 $HOME/governance2
 echo "ARGS=\"--tmp --dev --unsafe-rpc-external --unsafe-ws-external --rpc-methods unsafe --rpc-cors all\"" > $HOME/governance2.conf
 sudo systemctl restart governance2-testnet
