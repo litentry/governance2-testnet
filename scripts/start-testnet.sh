e87f52f785a804be05d7bcfa959be0084e6d6f42
zongxiong.chen <czxczf@gmail.com>
2022-06-12 20:40:08 +0200

    change files location

--- scripts/start-testnet.sh
+++ scripts/start-testnet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 $HOME/.cargo/bin/cargo build --release
-ln -sf $HOME/actions-runner/_work/governance2-testnet/governance2-testnet/release/governance2 /opt/governance2
-echo "ARGS=\"--tmp --dev --unsafe-rpc-external --unsafe-ws-external --rpc-methods unsafe --rpc-cors all\"" > /opt/governance2.conf
+ln -sf $HOME/actions-runner/_work/governance2-testnet/governance2-testnet/release/governance2 $HOME/governance2
+echo "ARGS=\"--tmp --dev --unsafe-rpc-external --unsafe-ws-external --rpc-methods unsafe --rpc-cors all\"" > $HOME/governance2.conf
 sudo systemctl restart governance2-testnet
