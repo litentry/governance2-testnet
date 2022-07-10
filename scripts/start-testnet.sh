7779139390111b78f63b39c8da404928a59b84d6
zongxiong.chen <czxczf@gmail.com>
2022-07-10 23:12:29 +0200

    run as archive node

--- scripts/start-testnet.sh
+++ scripts/start-testnet.sh
@@ -2,5 +2,5 @@
 
 $HOME/.cargo/bin/cargo build --release
 ln -sf $HOME/actions-runner/_work/governance2-testnet/governance2-testnet/target/release/governance2 $HOME/governance2
-echo "ARGS=\"--tmp --dev --unsafe-rpc-external --unsafe-ws-external --rpc-methods unsafe --rpc-cors all --prometheus-external\"" > $HOME/governance2.conf
+echo "ARGS=\"--name \\\"Governance2 Testnet\\\" --force-authoring --chain dev --alice  --pruning archive --unsafe-rpc-external --unsafe-ws-external --rpc-methods unsafe --rpc-cors all --prometheus-external\"" > $HOME/governance2.conf
 sudo systemctl restart governance2-testnet
